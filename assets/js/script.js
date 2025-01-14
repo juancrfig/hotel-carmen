6ffc265d5efd543724d3b3968afb2a6df48cd0c2
juancrfig <juancrfig@gmail.com>
2025-01-14 12:54:19 -0500

    Add two galleries

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// NAV MENU 
+
 const navButton = document.querySelector('.nav-button');
 const navMenu = document.querySelector('.nav-menu');
 const overlay = document.querySelector('.overlay');
@@ -9,3 +11,23 @@ function toggleMenu() {
   navMenu.classList.toggle('active');
   overlay.classList.toggle('active');
 }
+
+
+// GALLERY
+
+const gallery = document.querySelector('.gallery-wrapper');
+const gallerySpeed = 1; // Speed factor for scrolling
+
+let scrollAmount = 0;
+const step = () => {
+    scrollAmount += gallerySpeed;
+    if (scrollAmount >= gallery.scrollWidth / 2) {
+        scrollAmount = 0; // Reset scrolling to achieve infinite scrolling
+    }
+    gallery.style.transform = `translateX(${-scrollAmount}px)`;
+    requestAnimationFrame(step);
+};
+
+// Disable default animation if JavaScript is enabled
+gallery.style.animation = 'none';
+requestAnimationFrame(step);
